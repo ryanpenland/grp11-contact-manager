6c9deaa4ba87eee06ccaa186817bc9af5a737db1
JBalila <91769667+JBalila@users.noreply.github.com>
2022-09-17 12:58:03 -0400

    Found bug where code.js code seemingly not linked to contacts.html

--- js/code.js
+++ js/code.js
@@ -1,11 +1,31 @@
 const urlBase = "http://grp11.me/LAMPAPI";
 const extension = "php";
 
+// For User login
 let userId = 0;
 let firstName = "";
 let lastName = "";
+
+// For loading in Contacts
 let search = "";
-let currentPage = 2;
+let currentPage = 1;
+let LIMIT = 10;
+let offset = (currentPage - 1) * LIMIT;
+
+// function addLoadEvent(func) {
+//    var oldonload = window.onload();
+//    if (typeof window.onload != 'function') {
+//       window.onload = func;
+//    }
+//    else {
+//       window.onload = function() {
+//          if (oldonload) {
+//             oldonload();
+//          }
+//          func();
+//       }
+//    }
+// }
 
 function doLogin() {
   userId = 0;
@@ -62,12 +82,7 @@ function doRegister() {
 
   document.getElementById("registerResult").innerHTML = "";
 
-  let tmp = {
-    firstName: firstName,
-    lastName: lastName,
-    login: login,
-    password: password,
-  };
+  let tmp = {firstName: firstName, lastName: lastName, login: login, password: password};
   //	var tmp = {login:login,password:hash};
   let jsonPayload = JSON.stringify(tmp);
 
@@ -99,6 +114,8 @@ function saveCookie() {
     lastName +
     ",userId=" +
     userId +
+    ",currentPage=" +
+    currentPage +
     ";expires=" +
     date.toGMTString();
 }
@@ -107,17 +124,16 @@ function readCookie() {
   userId = -1;
   let data = document.cookie;
   let splits = data.split(",");
+
   for (var i = 0; i < splits.length; i++) {
     let thisOne = splits[i].trim();
     let tokens = thisOne.split("=");
-    if (tokens[0] == "firstName") {
-      firstName = tokens[1];
-    } else if (tokens[0] == "lastName") {
-      lastName = tokens[1];
-    } else if (tokens[0] == "userId") {
-      userId = parseInt(tokens[1].trim());
+
+    if (tokens[0] == "firstName") firstName = tokens[1];
+    else if (tokens[0] == "lastName") lastName = tokens[1];
+    else if (tokens[0] == "userId") userId = parseInt(tokens[1].trim());
+    else if (tokens[0] == "currentPage") currentPage = parseInt(tokens[1].trim());
     }
-  }
 
   if (userId < 0) {
     window.location.href = "index.html";
@@ -139,16 +155,11 @@ function resetTable() {
    let rowCount = table.rows.length;
    for (let i = 1; i < rowCount; i++)
       table.deleteRow(i);
+   window.alert(currentPage);
 }
 
 function loadContacts() {
-   resetTable();
-
    // currentPage = document.getElementById("page-number").value;    NEED TO IMPLEMENT PAGE-NUMBER SYSTEM
-   let limit = 10;
-   let offset = (currentPage - 1) * limit;
-   search = '';
-
    let table = document.getElementById("contact-list");
    let numContacts = 0;
 
@@ -245,42 +256,42 @@ function searchContacts() {
 
 }
 
-function searchColor() {
-  let srch = document.getElementById("searchText").value;
-  document.getElementById("colorSearchResult").innerHTML = "";
-
-  let colorList = "";
-
-  let tmp = { search: srch, userId: userId };
-  let jsonPayload = JSON.stringify(tmp);
-
-  let url = urlBase + "/SearchColors." + extension;
-
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  try {
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("colorSearchResult").innerHTML =
-          "Color(s) has been retrieved";
-        let jsonObject = JSON.parse(xhr.responseText);
-
-        for (let i = 0; i < jsonObject.results.length; i++) {
-          colorList += jsonObject.results[i];
-          if (i < jsonObject.results.length - 1) {
-            colorList += "<br />\r\n";
-          }
-        }
-
-        document.getElementsByTagName("p")[0].innerHTML = colorList;
-      }
-    };
-    xhr.send(jsonPayload);
-  } catch (err) {
-    document.getElementById("colorSearchResult").innerHTML = err.message;
-  }
-}
+// function searchColor() {
+//   let srch = document.getElementById("searchText").value;
+//   document.getElementById("colorSearchResult").innerHTML = "";
+//
+//   let colorList = "";
+//
+//   let tmp = { search: srch, userId: userId };
+//   let jsonPayload = JSON.stringify(tmp);
+//
+//   let url = urlBase + "/SearchColors." + extension;
+//
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("POST", url, true);
+//   xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+//   try {
+//     xhr.onreadystatechange = function () {
+//       if (this.readyState == 4 && this.status == 200) {
+//         document.getElementById("colorSearchResult").innerHTML =
+//           "Color(s) has been retrieved";
+//         let jsonObject = JSON.parse(xhr.responseText);
+//
+//         for (let i = 0; i < jsonObject.results.length; i++) {
+//           colorList += jsonObject.results[i];
+//           if (i < jsonObject.results.length - 1) {
+//             colorList += "<br />\r\n";
+//           }
+//         }
+//
+//         document.getElementsByTagName("p")[0].innerHTML = colorList;
+//       }
+//     };
+//     xhr.send(jsonPayload);
+//   } catch (err) {
+//     document.getElementById("colorSearchResult").innerHTML = err.message;
+//   }
+// }
 
 // ----------------- REGEX VALIDATIONS ----------------- //
 // Code courtesy of w3resource.com
